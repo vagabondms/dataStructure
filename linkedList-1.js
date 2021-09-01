69ed6efd516dc5e2191d7ac830df4e5069cb5624
김민석 <min@gimminseog-ui-MacBookPro.local>
2021-09-02 07:26:39 +0900

    two-way linkedlist

--- linkedList-1.js
+++ linkedList-1.js
@@ -37,6 +37,37 @@ class linkedList {
        }
        this.size++
     }
+    clearList(){
+        this.head = null
+        this.tail = null
+    }
+    removeFirst(){
+        if(!this.isEmpty()){
+            if(this.size === 1){
+                this.clearList()
+            } else {
+                this.head = this.head.next
+            }
+            this.size --
+        }
+    }
+    removeLast(){
+        if(!this.isEmpty()){
+            if(this.size === 1){
+                this.clearList()
+            } else {
+                let counter = this.size - 2
+                let cur = this.head
+                while(counter !== 0){
+                    cur = cur.next
+                    counter -- 
+                }
+                this.tail = cur
+                this.tail.next = null
+            }
+            this.size --
+        }
+    }
     insertAt(value, index){
         // 1) [예외] this.size < index 조건에 해당되면 에러처리가 필요하다.
         if(this.size < index || index < 0){
@@ -45,18 +76,63 @@ class linkedList {
             console.log('error')
             return
         }
-        if(this.isEmpty()){
-            this.insertFirst(value)
+
+        if(this.isEmpty() || index === 0){
+            // 2) 만약 비어있거나 index === 0 이면 insertFirst를 해야하거나, 하는 것이다.
+            this.insertFirst(value) 
         } else if(this.size === index) {
+            // 3) this.size === index라는 것은 마지막에다가 넣으라는 뜻이다. 
             this.insertLast(value)
         } else {
-            let counter = index - 1 
-            // 3) 이외의 경우는 while문을 통해 접근이 가능하다.
+            // 4) 모든 경우의 수를 거르고 이제 중간에 삽입하는 경우만 남았다.
+            let cur = this.head
+            let counter = index - 1
+            while (counter !== 0 ){
+                cur = cur.next
+                counter --
+            }
+            const prevNext = cur.next
+            cur.next = new Node(value)
+            cur.next.next = prevNext
+            this.size ++
         }
-        this.size ++
     }
+    // 특정 인덱스를 받아 해당 인덱스에 위치한 노드를 지워야한다. 
     removeAt(index){
-
+        // 1. 예외처리가 필요하다. 
+        if(index < 0 || this.size <= index ){
+            console.log('inappropriate inputs')
+            return
+        }
+        if(this.isEmpty()){
+            console.log('this is Empty')
+            return
+        }
+        if(this.size === 1){
+            this.head = null
+            this.tail = null
+            this.size --
+            return
+        }
+        if(index === 0 ){
+            this.head = this.head.next
+            this.size --
+            return 
+        }
+        let cur = this.head
+        let counter = index - 1 
+        while(counter !== 0 ){
+            cur = cur.next
+            counter -- 
+        }
+        if(cur.next.next){
+            cur.next = cur.next.next 
+            this.size --
+        } else {
+            cur.next = null
+            this.tail = cur
+            this.size --
+        }
     }
     isEmpty(){
         return this.size === 0
--- linkedList-2.js
+++ linkedList-2.js
@@ -9,73 +9,132 @@ class Node {
 class linkedList {
     constructor(){
         this.head = null
-        this.size = 0
+        this.tail = null
+        this.size = 0 
     }
-    isValid(index){
-        if(index < 0 || this.size < index){
-            return false
+    isEmpty(){
+        return this.size === 0
+    }
+    clear(){
+        this.head = null
+        this.tail = null
+        this.size = 0 
+    }
+    addToHead(value){
+        let newNode = new Node(value)
+        if(this.isEmpty()){
+            this.head = newNode
+            this.tail = newNode 
+        } else {
+            let prevHead = this.head
+            this.head = newNode
+            this.head.next = prevHead
+            prevHead.prev = this.head
         }
-        return true
-    }
-    insertFirst(value){
-        const prevHead = this.head
-        this.head = new Node(value, prevHead)
         this.size ++
     }
-    insertLast(value){
-        let cur = this.head
-        while(cur.next){
-            cur = cur.next
+    addToTail(value){
+        let newNode = new Node(value)
+        if(this.isEmpty()){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            let prevTail = this.tail
+            this.tail = newNode
+            prevTail.next = this.tail
+            this.tail.prev = prevTail
         }
-        cur.next = new Node(value)
-        this.size++
+        this.size ++
     }
-    insertAt(value, index){
-
-        if(!this.isValid(index)){
+    removeHead(){
+        if(this.isEmpty()){
+            console.log('is Empty')
+        } else if(this.size === 1 ) {
+            this.clear()
+            this.size --
+        } else {
+            this.head = this.head.next
+            this.head.prev = null
+            this.size --
+        }
+    }
+    removeTail(){
+        if(this.isEmpty()){
+            console.log('is Empty')
+        } else if(this.size === 1 ) {
+            this.clear()
+            this.size --
+        } else {
+            this.tail = this.tail.prev
+            this.tail.next = null
+            this.size--
+        }
+    }
+    addAt(value,index){
+        if(this.isEmpty() || index < 0 || this.size < index){
+            console.log('inappropriate')
             return
         }
-
+        
+        let cur = this.head
+        let newNode = new Node(value)
         if(index === 0){
-            this.insertFirst(value)
+            
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+
         } else {
-            let cur = this.head
-            let leftIndex = index - 1 
-            while (leftIndex !== 0 ){
+            while(index !== 0 ){
                 cur = cur.next
-                leftIndex --
+                index --  
             }
-            let nextNode = cur.next
-            cur.next = new Node(value,nextNode)
+            
+            if(cur){
+                let prevNode = cur.prev
+                prevNode.next = newNode
+                newNode.prev = prevNode
+                
+                cur.prev = newNode
+                newNode.next = cur
+                
+            } else {
+                this.tail.next = newNode
+                newNode.prev = this.tail
+                this.tail = newNode
+            }
         }
+        
         this.size ++
     }
     removeAt(index){
-        // this could be a function 
-        if(!this.isValid(index)){
+        if(this.isEmpty() || index < 0 || this.size <= index){
+            console.log('inappropriate')
             return
         }
-        // if(index < 0 || this.size <= index){
-        //     console.log('Out of Range')
-        //     return
-        // }
-
-        
+        if(this.size === 1){
+            this.clear()
+            return
+        }
         if(index === 0){
-            this.head = this.head.next   
+            this.head = this.head.next 
+            this.head.prev = null
+        } else if(this.size - 1 === index){
+            let prevNode = this.tail.prev
+            prevNode.next = null
+            this.tail = prevNode 
         } else {
-           let leftIndex = index - 1 
-           let cur = this.head
-           while (leftIndex !== 0 ){
-            cur = cur.next
-            leftIndex --
-           }
-           let nextNode = cur.next.next
-           cur.next = nextNode
-        }        
+            let cur = this.head
+            while(index !== 0 ){
+                cur = cur.next
+                index --  
+            }
+            let prevNode = cur.prev 
+            let nextNode = cur.next
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+        }
         this.size --
-    }
-    isEmpty(index){
-        return this.head === null
+        return
     }
 }
